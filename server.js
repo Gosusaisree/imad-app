73d4112a9e4a22dbef5649bf70c348b1d5a04b6c
Gosusaisree <36734357+Gosusaisree@users.noreply.github.com>
2018-02-22 23:15:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/sai-1', function (req, res) {
+app.get('/medha', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'medha.html')); 
 });
 
